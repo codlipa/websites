9beced7c1ebb5f213c5c62a067c3fa5801fb1eb3
thelabuzov <thelabuzov@gmail.com>
2022-01-03 09:04:40 -0500

    create 2 sections & slider

--- techgear/app/js/script.js
+++ techgear/app/js/script.js
@@ -1,5 +1,11 @@
-console.log('HELLO');
-
-const test = () => {
-	console.log('this is a test');
-};
+const swiper = new Swiper('.swiper', {
+	slidesPerView: 4,
+	spaceBetween: 20,
+	keyboard: {
+	  enabled: true
+	},
+	navigation: {
+	  nextEl: '.swiper-button-next',
+	  prevEl: '.swiper-button-prev'
+	}
+})
