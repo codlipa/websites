1e8b672ec49831b536c6cd200a35a31acc3a1b15
thelabuzov <thelabuzov@gmail.com>
2022-04-26 06:45:59 -0500

    modify slider pagination and create slides

--- travel/app/js/script.js
+++ travel/app/js/script.js
@@ -1,8 +1,13 @@
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper(".swiper", {
 	loop: true,
 
+	pagination: {
+		el: ".swiper-pagination",
+		type: "fraction",
+	},
+
 	navigation: {
-	  nextEl: '.swiper-button-next',
-	  prevEl: '.swiper-button-prev'
-	}
+		prevEl: ".swiper-button-prev",
+		nextEl: ".swiper-button-next",
+	},
 });
